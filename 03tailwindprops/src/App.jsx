cab2c8183d937abfab610abba62bbb4f87a4d574
Deepender <rohilladeepender543@gmail.com>
2026-06-26 13:04:53 +0530

    Updated project

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Card from './components/Card'
+
 function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Deepender",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
   return (
-    <div className="bg-red-500 text-white text-4xl p-10">
-      Tailwind Working
-    </div>
-  );
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+     
+      <Card username="Deepender" />
+    </>
+  )
 }
 
-export default App;
+export default App
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-function Card({ title, description, imageUrl }) {
+import React from 'react'
+
+function Card({username, btnText="visit me"}) {
+    console.log(username);
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={imageUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
-      </div>
-    </div>
-  );
+    <div className="relative h-[400px] w-[300px] rounded-md ">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    className="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">{username}</h1>
+    <p className="mt-2 text-sm text-gray-300">
+     Hi I am Learning Application Development.
+     I am Worked on Some  Projects.
+    </p>
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      {btnText } →
+    </button>
+  </div>
+</div>
+  )
 }
-export default Card;
+
+export default Card
